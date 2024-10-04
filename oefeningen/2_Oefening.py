50f4b39e859c61bb5d006be1c2aa8fa2bbddee2b
xanderpacquet <xander.pacquet@student.tsaam.be>
2024-10-04 12:12:45 +0200

    laatste commit

--- oefeningen/2_Oefening.py
+++ oefeningen/2_Oefening.py
@@ -1,7 +1,7 @@
-persoon = {"naam": "Peters", "voornaam" : "Jos", "leeftijd" : 45, "woonplaats": "stokstraat 13"}
+persoon = {"naam": "Peters", "voornaam" : "Jos", "leeftijd" : 45, "woonplaats": "stokstraat 13","beroep": "leerkracht"}
 print(persoon["naam"])
 print(persoon["voornaam"])
 print(persoon["leeftijd"])
 print(persoon["woonplaats"])
 persoon["beroep"] = "dokter "
-print(persoon.get("beroep", "niet gekend"))
+print(persoon)
---
+++ oefeningen/3_Oefening.py
@@ -0,0 +1,8 @@
+personen = [
+    {"naam": "Peter", "leeftijd": 18},
+    {"naam": "koopman", "leeftijd": 38},
+    {"naam": "pieters", "leeftijd": 44},
+]
+
+for persoon in personen:
+    print(f"naam: {persoon['naam']} , leeftijd {persoon['leeftijd']}")
